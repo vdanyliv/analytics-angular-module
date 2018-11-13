8633ac3a1b39ffa4e8173a463b638649eb41daeb
volodymyrdanyliv <vdanyliv.work@gmail.com>
2018-11-14 00:38:58 +0200

    module bootstrap

---
+++ src/app/components/event-check/event-check.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+import {NgxFsAnalyticsService} from '../../projects/ngx-fs-analytics/src/lib/services/ngx-fs-analytics.service';
+
+@Component({
+  selector: 'app-event-check',
+  templateUrl: 'event-check.component.html'
+})
+
+export class EventCheckComponent implements OnInit {
+  constructor(public analytics: NgxFsAnalyticsService) {
+  }
+
+  ngOnInit() {
+  }
+}
